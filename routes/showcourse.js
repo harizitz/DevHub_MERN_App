01f19116bde131da13627a2b446407772b886ee2
harizitz <43085260+harizitz@users.noreply.github.com>
2020-08-08 21:08:08 +0530

    descending

--- routes/showcourse.js
+++ routes/showcourse.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 let Course = require("../models/Course");
 router.route("/").get((req, res) => {
   Course.find()
+    .sort({ createdAt: -1 })
     .then((data) => res.json(data))
     .catch((err) => res.status(400).json("Error: " + err));
 });
--- routes/showhiring.js
+++ routes/showhiring.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 let Hire = require("../models/Hire");
 router.route("/").get((req, res) => {
   Hire.find()
+    .sort({ createdAt: -1 })
     .then((data) => res.json(data))
     .catch((err) => res.status(400).json("Error: " + err));
 });
