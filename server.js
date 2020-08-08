5741969367042c94a6686a98ec2a674bc0ccb8ea
harizitz <43085260+harizitz@users.noreply.github.com>
2020-08-08 20:43:55 +0530

    changes in server

--- server.js
+++ server.js
@@ -3,20 +3,18 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const path = require("path");
 require("dotenv").config();
+
 const app = express();
-const port = process.env.PORT || 5000;
-const serveHost = process.env.YOUR_HOST || "0.0.0.0";
 app.use(cors());
 app.use(express.json());
 
-if (process.env.NODE_ENV === "production") {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client','build')));
-  // Handle React routing, return all requests to React app
-  app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
-  });
-}
+const FillRouter = require("./routes/filldetail");
+const HireRouter = require("./routes/showhiring");
+const CourseRouter = require("./routes/showcourse");
+app.use("/api/filldetail", FillRouter);
+app.use("/api/hiring", HireRouter);
+app.use("/api/coupons", CourseRouter);
+
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, {
   useNewUrlParser: true,
@@ -29,12 +27,18 @@ connection.once("open", () => {
   console.log("MongoDB database connection established successfully");
 });
 
-const FillRouter = require("./routes/filldetail");
-const HireRouter = require("./routes/showhiring");
-const CourseRouter = require("./routes/showcourse");
-app.use("/api/filldetail", FillRouter);
-app.use("/api/hiring", HireRouter);
-app.use("/api/coupons", CourseRouter);
-app.listen(port,serveHost, () => {
+if (process.env.NODE_ENV === "production") {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, "client", "build")));
+  // Handle React routing, return all requests to React app
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
+
+const port = process.env.PORT || 5000;
+const serveHost = process.env.YOUR_HOST || "0.0.0.0";
+
+app.listen(port, serveHost, () => {
   console.log(`Server is running on port: ${port}`);
 });
